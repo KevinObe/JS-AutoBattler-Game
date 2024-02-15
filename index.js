b4ed7bb3445d78a1ba6f8958a6e6c236a1cbef4c
Kevin Oberluggauer <kevin.oberluggauer@gmail.com>
2024-02-15 16:50:10 +0100

    added function for the player to shoot and functions to detect hits. Also a function to respawn killed enemies and a score counter.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let gameSwitch = false;
 const game = document.querySelector('#game');
 const viewport = document.getElementById('viewport');
 const controlUnit = document.querySelector('.controlUnit');
+const score = document.querySelector('#score');
 
 const gameWidth = 2000;
 const gameHeight = 2000;
@@ -20,6 +21,10 @@ let gameCurrentPosition = gameStartPosition;
 const dialog = document.querySelector('dialog');
 const startBtn = document.querySelector('#startBtn');
 
+const gameSettings = {
+  counter: 0,
+}
+
 let gameInterval;
 
 // create player
@@ -27,6 +32,9 @@ const player = {
   range: 20,
   height: 30,
   width: 30,
+  speed: 50,
+  firingRate: 1000,
+  damage: 50,
 }
 
 // create enemies
@@ -40,6 +48,9 @@ class Enemy {
     this.topLeft = [xAxis, yAxis + enemyHeight];
     this.topRight = [xAxis + enemyWidth, yAxis + enemyHeight];
     this.startPosition = this.bottomLeft;
+    this.health = 100;
+    this.protection = false;
+    this.speed = 100;
   }
 }
 
@@ -52,7 +63,7 @@ const randomSpawnPoint = () => {
   }
   return [x, y];
 }
-
+let enemieNumber = 5;
 let enemies = []
 
 // player spwan / start game
@@ -60,6 +71,7 @@ function spawnPlayer() {
   if(gameSwitch){
     player.id = document.createElement('div');
     player.currentPosition = playerStartPosition;
+    player.direction = 'right';
     player.health = 100;
     player.lifeTime = () => {
       let lifeTimeInterval = setInterval(() => {
@@ -71,13 +83,25 @@ function spawnPlayer() {
         }
       }, 50);
     };
-    let enemieNumber = 10;
-    for(let i = 0; i <= enemieNumber; i++){
+    player.firing = () => {
+      let firingInterval = setInterval(() => {
+        if(gameSwitch){
+          createPlayerFire();
+        } else {
+          clearInterval(firingInterval);
+        }
+      }, player.firingRate);
+    };
+
+    for(let i = 0; i < enemieNumber; i++){
       enemies.push(new Enemy(randomSpawnPoint()));
     };
     dialog.close();
+    gameSettings.counter = 0;
+    score.textContent = `Score: ${gameSettings.counter}`;
     drawPlayer();
     spawnEnemy();
+    player.firing();
     gameInterval = setInterval(() => {
       getDistanceToPlayer();
     }, 500);
@@ -154,27 +178,31 @@ function movePlayer(direction) {
 function startMoving(direction) {
   moveInterval = setInterval(() => {
     movePlayer(direction);
-  }, 50);
+  }, player.speed);
 }
 
 
 arrowLeft.addEventListener('touchstart', (e) => {
   e.preventDefault();
+  player.direction = 'left';
   startMoving('left');
 });
 
 arrowRight.addEventListener('touchstart', (e) => {
   e.preventDefault();
+  player.direction = 'right';
   startMoving('right');
 });
 
 arrowDown.addEventListener('touchstart', (e) => {
   e.preventDefault();
+  player.direction = 'down';
   startMoving('down');
 });
 
 arrowUp.addEventListener('touchstart', (e) => {
   e.preventDefault();
+  player.direction = 'up';
   startMoving('up');
 });
 
@@ -183,8 +211,124 @@ document.ontouchend = () => {
 }
 
 // player firing
+let shots = [];
+
+class Shot {
+  constructor(xAxis, yAxis) {
+    this.width = 15;
+    this.height = 15;
+    this.bottomLeft = [xAxis, yAxis];
+    this.topLeft = [xAxis, yAxis + this.height]; // topright instead .......
+  }
+}
+
+function createPlayerFire() {
+  let shot = new Shot(playerCurrentPosition[0] + (player.width / 4), playerCurrentPosition[1] + (player.height / 4));
+  shot.id = document.createElement('div');
+  shot.id.className = 'shots';
+  shot.id.style.left = `${shot.bottomLeft[0]}px`;
+  shot.id.style.bottom = `${shot.bottomLeft[1]}px`;
+  shots.push(shot);
+  game.appendChild(shot.id);
+  playerAutoFiring(shot);
+}
+
+function playerAutoFiring(shot) {
+  if(player.direction === 'right'){
+    shot.interval = setInterval(() => {
+      shot.bottomLeft[0] += 10;
+      drawShot(shot);
+    }, 30);
+  } else if(player.direction === 'left'){
+    shot.interval = setInterval(() => {
+      shot.bottomLeft[0] -= 10;
+      drawShot(shot);
+    }, 30);
+  } else if(player.direction === 'down'){
+    shot.interval = setInterval(() => {
+      shot.bottomLeft[1] -= 10;
+      drawShot(shot);
+    }, 30);
+  } else if(player.direction === 'up'){
+    shot.interval = setInterval(() => {
+      shot.bottomLeft[1] += 10;
+      drawShot(shot);
+    }, 30);
+  } else {
+    return;
+  }
+}
+
+function drawShot(shot) {
+  shot.id.style.left = `${shot.bottomLeft[0]}px`;
+  shot.id.style.bottom = `${shot.bottomLeft[1]}px`;
+  checkTargetHits(shot);
+}
 
 // target hit collisions
+function checkTargetHits(shot) {
+  if(shot.bottomLeft[0] > 2000 || shot.bottomLeft[0] < 0 || shot.bottomLeft[1] > 2000 || shot.bottomLeft[1] < 0){
+    let index = shots.findIndex((arrayShot) => arrayShot.id === shot.id);
+    clearInterval(shot.interval);
+    shot.id.remove();
+    if(index !== -1){
+      shots.splice(index, 1);
+    }
+    return;
+  }
+
+  for(const enemy of enemies){
+    if(enemy.protection) return;
+    if(((shot.bottomLeft[0] > enemy.bottomLeft[0] && shot.bottomLeft[0] < enemy.bottomRight[0])
+      &&
+      (shot.bottomLeft[1] > enemy.bottomLeft[1] && shot.bottomLeft[1] < enemy.topLeft[1]))
+      ||
+      ((shot.topLeft[0] > enemy.bottomLeft[0] && shot.topLeft[0] < enemy.bottomRight[0])
+      &&
+      (shot.topLeft[1] > enemy.bottomLeft[1] && shot.topLeft[1] < enemy.topLeft[1]))
+      ){
+        console.log('hit')
+      let index = shots.findIndex((arrayShot) => arrayShot.id === shot.id);
+      clearInterval(shot.interval);
+      shot.id.remove();
+      if(index !== -1){
+        shots.splice(index, 1);
+      }
+      enemy.health -= player.damage;
+      enemy.protection = true;
+      setTimeout(() => {
+        enemy.protection = false;
+      }, 3000);
+      if(enemy.health <= 0){
+        killEnemy(enemy);
+      };
+      return;
+    }
+  }
+  return;
+}
+
+function killEnemy(enemy){
+  let index = enemies.findIndex((remainingEnemies) => remainingEnemies.id === enemy.id)
+  if(index === -1) return;
+  enemy.id.remove();
+  clearInterval(enemy.interval);
+  enemies.splice(index, 1);
+  gameSettings.counter ++;
+  score.textContent = `Score: ${gameSettings.counter}`;
+
+  if(enemies.length <= 0 || gameSettings.counter >= 50) {
+    resetGame();
+  } else {
+    let enemy = new Enemy(randomSpawnPoint());
+    enemy.id = document.createElement('div');
+    enemy.id.className = 'enemy';
+    enemies.push(enemy);
+    drawEnemy(enemy);
+    game.appendChild(enemy.id);
+  }
+  return;
+}
 
 
 //spawn enemies
@@ -314,7 +458,7 @@ function getDistanceToPlayer(){
     }
     enemy.interval = setInterval(() => {
       moveToPlayer(enemy);
-     }, 100);
+     }, enemy.speed);
   }
 }
 
@@ -388,6 +532,14 @@ function resetGame() {
   delete player.id;
   clearInterval(player.lifeTime);
   delete player.lifeTime;
+  delete player.direction;
+
+  //shots
+  for(const shot of shots) {
+    clearInterval(shot.interval);
+    shot.id.remove();
+  }
+  shots = [];
 
   //enemies
   for(const enemy of enemies) {
